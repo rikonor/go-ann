579f35aa236eac3b68be254ff39f498f217aeedc
Or Rikon <rikonor@gmail.com>
2017-07-23 20:50:31 -0400

    MRPT tree building done

--- mrpt.go
+++ mrpt.go
@@ -1,206 +1,211 @@
 package ann
 
-// type mrpt struct {
-// 	trees []*tree
+import (
+	"math/rand"
+	"sort"
+
+	mat "github.com/gonum/matrix/mat64"
+)
+
+type mrpt struct {
+	xs    [][]float64
+	trees []*tree
+}
+
+type tree struct {
+	root *node
+	r    mat.Matrix
+}
+
+type node struct {
+	split float64
+	left  *node
+	right *node
+
+	indices []int
+}
+
+// NewMRPTANNer creates a NN index using random projection trees
+// See https://arxiv.org/pdf/1509.06957.pdf for additional details
+// t -> number of trees, l -> depth of tree
+func NewMRPTANNer(t int, l int, xs [][]float64) ANNer {
+	a := 0.5 // TODO(temporary)
+
+	// Infer dimensions from input matrix
+	n, d := len(xs), len(xs[0])
+
+	// Convert xs to a gonum matrix
+	X := mat.NewDense(d, n, nil)
+	for i := 0; i < n; i++ {
+		X.SetCol(i, xs[i])
+	}
+
+	return &mrpt{
+		xs:    xs,
+		trees: growTrees(X, t, l, a),
+	}
+}
+
+func growTrees(X mat.Matrix, t, l int, a float64) []*tree {
+	// feature vector dimension
+	d, n := X.Dims()
+
+	trees := []*tree{}
+
+	// Create t trees
+	for i := 0; i < t; i++ {
+		// Create a RP matrix
+		R := mat.NewDense(d, l-1, nil)
+		// Create one random vector per tree level
+		for j := 0; j < (l - 1); j++ {
+			rs := []float64{}
+
+			for k := 0; k < d; k++ {
+				// TODO: only use non-zero value with prob a
+
+				// TODO: Use a sparse vector strategy
+				rs = append(rs, rand.NormFloat64())
+			}
+
+			// Set the random vector into the matrix
+			R.SetCol(j, rs)
+		}
+
+		// Calculate the projections
+		var P mat.Dense
+		P.Mul(X.T(), R)
+
+		// Create indices list
+		indices := []int{}
+		for i := 0; i < n; i++ {
+			indices = append(indices, i)
+		}
+
+		trees = append(trees, &tree{
+			r:    R,
+			root: growTree(&P, l, 0, indices),
+		})
+	}
+
+	return trees
+}
+
+func growTree(P mat.Matrix, l, level int, indices []int) *node {
+	// Stop if we're at the leaf
+	if level == l-1 {
+		return &node{indices: indices}
+	}
+
+	// Get the projections for this level
+	ps := mat.Col(nil, level, P)
+
+	// Get the median of the projections
+	m := median(ps)
+
+	// Divide indices to left and right based on median value
+	leftIndices := []int{}
+	rightIndices := []int{}
+	for _, i := range indices {
+		if ps[i] <= m {
+			leftIndices = append(leftIndices, i)
+		} else {
+			rightIndices = append(rightIndices, i)
+		}
+	}
+
+	return &node{
+		left:  growTree(P, l, level+1, leftIndices),
+		right: growTree(P, l, level+1, rightIndices),
+		split: m,
+	}
+}
+
+func (nn *mrpt) ANN(q []float64, k int) []int {
+	// Keep track of votes
+	votesMap := map[int]int{}
+
+	// How many votes does a vector need to be included in the output set
+	reqVotes := 1
+
+	// Query the trees to get candidates
+	for _, tree := range nn.trees {
+		indices := queryTree(tree, q, k)
+		for _, i := range indices {
+			// Count vote
+			votesMap[i]++
+		}
+	}
+
+	xsCandidates := [][]float64{}
+	xsIndices := []int{}
+	for i, votes := range votesMap {
+		if votes >= reqVotes {
+			xsCandidates = append(xsCandidates, nn.xs[i])
+			// Track the index of each vector so we can retrieve it later
+			xsIndices = append(xsIndices, i)
+		}
+	}
+
+	// Perform naive k-nearest-neighbor search on candidates set
+	knn := NewExhaustiveNNer(xsCandidates)
+	knnIndices := knn.ANN(q, k)
+
+	// Convert the above knnIndices to the indices of the vectors in the scope
+	// of all of our data
+	indices := []int{}
+	for _, i := range knnIndices {
+		indices = append(indices, xsIndices[i])
+	}
+
+	return indices
+}
+
+func queryTree(tree *tree, q []float64, k int) []int {
+	return []int{}
+	// fmt.Printf("\nLooking for NN for %v\n", q)
+	//
+	// // Get vector dimension and depth of tree
+	// d, l := tree.r.Dims()
+	//
+	// // Convert q to a vector so we can perform matrix math
+	// qv := mat64.NewVector(d, q)
+	//
+	// // Project query point onto tree's random matrix
+	// var p mat64.Vector
+	// p.MulVec(tree.r.T(), qv)
+	//
+	// // Traverse the tree until point lands in a bucket
+	// node := tree.root
+	// for i := 0; i < l; i++ {
+	// 	if p.At(i, 0) <= node.split {
+	// 		node = node.left
+	// 	} else {
+	// 		node = node.right
+	// 	}
+	// }
+	//
+	// return node.xs
+}
+
+// func querySubtree(n *node, q []float64, k int) []int {
+//
 // }
-//
-// type tree struct {
-// 	root *node
-// 	r    mat64.Matrix
-// }
-//
-// type node struct {
-// 	split float64
-// 	left  *node
-// 	right *node
-//
-// 	xs [][]float64
-// }
-//
-// // NewMRPTNNer creates a NN index using random projection trees
-// // See https://arxiv.org/pdf/1509.06957.pdf for additional details
-// // t -> number of trees, l -> depth of tree
-// func NewMRPTNNer(t int, l int, xs [][]float64) NNer {
-// 	a := 0.5 // TODO(temporary)
-// 	return &mrpt{trees: growTrees(xs, t, l, a)}
-// }
-//
-// func growTrees(xs [][]float64, t int, l int, a float64) []*tree {
-// 	// Number of vectors
-// 	n := len(xs)
-//
-// 	// Infer vector dimension from xs
-// 	d := len(xs[0])
-//
-// 	// Convert xs to a matrix
-// 	// TODO should remove this X thing
-// 	X := mat64.NewDense(d, n, nil)
-// 	for i := 0; i < n; i++ {
-// 		X.SetCol(i, xs[i])
-// 	}
-// 	fmt.Printf("Vectors:\n%v\n", mat64.Formatted(X))
-//
-// 	fmt.Printf("Starting to grow %d trees of depth %d for %d vectors [a=%f]:\n",
-// 		t, len(xs), l, a)
-//
-// 	trees := []*tree{}
-//
-// 	for i := 0; i < t; i++ {
-// 		fmt.Printf("Building a random projection matrix with %d %d-dimensional vectors\n",
-// 			l-1, d)
-//
-// 		// Create a new random projection matrix
-// 		r := mat64.NewDense(d, l-1, nil)
-// 		// Create one random vector per tree level
-// 		for j := 0; j < (l - 1); j++ {
-// 			vs := []float64{}
-//
-// 			for k := 0; k < d; k++ {
-// 				// TODO: Use a sparse vector strategy
-// 				vs = append(vs, rand.NormFloat64())
-// 			}
-//
-// 			// Set the random vector into the matrix
-// 			r.SetCol(j, vs)
-// 		}
-//
-// 		fmt.Printf("RP Matrix #%d:\n%v\n",
-// 			i, mat64.Formatted(r))
-//
-// 		// Create a new tree
-// 		trees = append(trees, &tree{
-// 			r:    r,
-// 			root: growTree(xs, l, 0, r),
-// 		})
-// 	}
-//
-// 	return trees
-// }
-//
-// // growTree is a recursive function for building a RP tree
-// // xs -> points
-// // r -> random projection matrix
-// func growTree(xs [][]float64, l, level int, r *mat64.Dense) *node {
-// 	fmt.Printf("\nBuilding tree level %d\n", level)
-// 	if level == l-1 {
-// 		fmt.Printf("Returning leaf with vectors: %v\n", xs)
-// 		return &node{xs: xs}
-// 	}
-//
-// 	// Get the random projection vector of the current level
-// 	rv := r.ColView(level)
-//
-// 	fmt.Printf("Calculating projections using RP vector:\n%v\n", mat64.Formatted(rv))
-// 	projVals := []float64{}
-// 	for _, x := range xs {
-// 		// Convert x to a vector
-// 		xv := mat64.NewVector(len(x), x)
-//
-// 		// Get projection
-// 		projVals = append(projVals, mat64.Dot(xv, rv))
-// 	}
-// 	fmt.Println("Projections for this RP vector:", projVals)
-//
-// 	// Get the median of the projections
-// 	split := median(projVals)
-//
-// 	// Put vectors in left or right subtree
-// 	// based on which side of the split their project falls
-// 	xsLeft := [][]float64{}
-// 	xsRight := [][]float64{}
-//
-// 	for i, proj := range projVals {
-// 		fmt.Printf("Projection for %v: %f\n", xs[i], proj)
-// 		if proj <= split {
-// 			xsLeft = append(xsLeft, xs[i])
-// 		} else {
-// 			xsRight = append(xsRight, xs[i])
-// 		}
-// 	}
-// 	fmt.Printf("Left node: %v\n", xsLeft)
-// 	fmt.Printf("Right node: %v\n", xsRight)
-//
-// 	return &node{
-// 		split: split,
-// 		left:  growTree(xsLeft, l, level+1, r),
-// 		right: growTree(xsRight, l, level+1, r),
-// 	}
-// }
-//
-// func (nn *mrpt) NN(q []float64) []float64 {
-// 	// Keep candidates in a set
-// 	xsSet := map[string][]float64{}
-// 	votes := map[string]int{}
-//
-// 	// How many votes does a vector need to be included in the output set
-// 	reqVotes := 1
-//
-// 	// Query the trees to get candidates
-// 	for _, tree := range nn.trees {
-// 		xs := queryTree(tree, q)
-// 		for _, x := range xs {
-// 			// Count vote
-// 			k := fmt.Sprintf("%v", x)
-// 			votes[k]++
-//
-// 			// If vector has enough votes, include in output set
-// 			if votes[k] == reqVotes {
-// 				xsSet[k] = x
-// 			}
-// 		}
-// 	}
-//
-// 	xsCandidates := [][]float64{}
-// 	for _, x := range xsSet {
-// 		xsCandidates = append(xsCandidates, x)
-// 	}
-//
-// 	// Perform naive k-nearest-neighbor search on candidates set
-// 	knn := NewExhaustiveNNer(xsCandidates)
-// 	return knn.NN(q)
-// }
-//
-// func queryTree(tree *tree, q []float64) [][]float64 {
-// 	fmt.Printf("\nLooking for NN for %v\n", q)
-//
-// 	// Get vector dimension and depth of tree
-// 	d, l := tree.r.Dims()
-//
-// 	// Convert q to a vector so we can perform matrix math
-// 	qv := mat64.NewVector(d, q)
-//
-// 	// Project query point onto tree's random matrix
-// 	var p mat64.Vector
-// 	p.MulVec(tree.r.T(), qv)
-//
-// 	// Traverse the tree until point lands in a bucket
-// 	node := tree.root
-// 	for i := 0; i < l; i++ {
-// 		if p.At(i, 0) <= node.split {
-// 			node = node.left
-// 		} else {
-// 			node = node.right
-// 		}
-// 	}
-//
-// 	return node.xs
-// }
-//
-// // median calculates the median value of a series of elements
-// func median(vals []float64) float64 {
-// 	// Make a copy so we don't alter the given slice
-// 	vs := make([]float64, len(vals))
-// 	copy(vs, vals)
-//
-// 	sort.Float64s(vs)
-//
-// 	// Even number of elements
-// 	if len(vs)%2 == 0 {
-// 		m := (vs[len(vs)/2-1] + vs[len(vs)/2]) / 2
-// 		return m
-// 	}
-//
-// 	// Odd number of elements
-// 	m := vs[len(vs)/2]
-// 	return m
-// }
+
+// median calculates the median value of a series of elements
+func median(vals []float64) float64 {
+	// Make a copy so we don't alter the given slice
+	vs := make([]float64, len(vals))
+	copy(vs, vals)
+
+	sort.Float64s(vs)
+
+	// Even number of elements
+	if len(vs)%2 == 0 {
+		m := (vs[len(vs)/2-1] + vs[len(vs)/2]) / 2
+		return m
+	}
+
+	// Odd number of elements
+	m := vs[len(vs)/2]
+	return m
+}
---
+++ mrpt_test.go
@@ -0,0 +1,45 @@
+package ann
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMRPTNNer(t *testing.T) {
+	nn := NewMRPTANNer(1, 3, [][]float64{
+		[]float64{0, 0, 0, 0},
+		[]float64{1.1, 1.1, 1.1, 1.1},
+		[]float64{2, 2, 2, 2},
+		[]float64{-1, 1, 2, 2},
+		[]float64{2, 2, -1, 2},
+		[]float64{2, 3, 2, -4},
+		[]float64{-2, -3, 2, -4},
+		[]float64{-2, -3, -2, 4},
+	})
+
+	q := []float64{1, 1, 1, 1}
+	indices := nn.ANN(q, 1)
+
+	// Should be closest to vector #1
+	fmt.Println(indices)
+}
+
+func TestMedian(t *testing.T) {
+	type testCase struct {
+		vs []float64
+		m  float64
+	}
+
+	testCases := []testCase{
+		testCase{vs: []float64{1}, m: 1},
+		testCase{vs: []float64{1, 2, 3}, m: 2},
+		testCase{vs: []float64{1, 2, 3, 4}, m: 2.5},
+	}
+
+	for _, tc := range testCases {
+		m := median(tc.vs)
+		if m != tc.m {
+			t.Fatalf("wrong median: %f, expected %f", m, tc.m)
+		}
+	}
+}

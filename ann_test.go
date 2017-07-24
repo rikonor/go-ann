455e09331b34b272b5e3ea2346e42ab753777474
Or Rikon <rikonor@gmail.com>
2017-07-24 19:51:10 -0400

    Add some basic benchmarking

--- ann_test.go
+++ ann_test.go
@@ -1,31 +1,54 @@
 package ann
 
-// func TestExhaustiveSearch(t *testing.T) {
-// 	nn := NewExhaustiveNNer([][]float64{
-// 		[]float64{0, 0, 0},
-// 		[]float64{1.1, 1.1, 1.1},
-// 		[]float64{2, 2, 2},
-// 	})
-//
-// 	p := nn.NN([]float64{1, 1, 1})
-//
-// 	if !reflect.DeepEqual(p, []float64{1.1, 1.1, 1.1}) {
-// 		t.Fatalf("Found wrong nearest neighbor: %v", p)
-// 	}
-// }
-//
-// func TestMRPTNNerNew(t *testing.T) {
-// 	nn := NewMRPTNNer(1, 3, [][]float64{
-// 		[]float64{1.1, 1.1, 1.1, 1.1},
-// 		[]float64{0, 0, 0, 0},
-// 		[]float64{2, 2, 2, 2},
-// 		[]float64{3, 3, 3, 3},
-// 		[]float64{4, 4, 4, 4},
-// 		[]float64{5, 5, 5, 5},
-// 	})
-//
-// 	q := []float64{2.9, 2.9, 2.9, 2.9}
-// 	fmt.Printf("Querying for %v\n", q)
-// 	p := nn.NN(q)
-// 	fmt.Printf("\nResult: %v\n", p)
-// }
+import (
+	"math/rand"
+	"testing"
+)
+
+func BenchmarkMRPTANNer(b *testing.B) {
+	n := 10000
+	d := 100
+
+	nn := NewMRPTANNer(3, 10, randomMatrix(n, d))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		q := randomVector(d)
+		b.StartTimer()
+
+		nn.ANN(q, 1)
+	}
+}
+
+func BenchmarkExhaustiveNNer(b *testing.B) {
+	n := 10000
+	d := 100
+
+	nn := NewExhaustiveNNer(randomMatrix(n, d))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		q := randomVector(d)
+		b.StartTimer()
+
+		nn.ANN(q, 1)
+	}
+}
+
+func randomMatrix(n, d int) [][]float64 {
+	xs := [][]float64{}
+	for i := 0; i < n; i++ {
+		xs = append(xs, randomVector(d))
+	}
+	return xs
+}
+
+func randomVector(d int) []float64 {
+	vs := []float64{}
+	for j := 0; j < d; j++ {
+		vs = append(vs, rand.NormFloat64())
+	}
+	return vs
+}

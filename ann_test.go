93bdf2e1988a5a84140109a1e61f9fdb71f0c030
Or Rikon <rikonor@gmail.com>
2017-07-24 22:02:33 -0400

    Add an accuracy check

--- ann_test.go
+++ ann_test.go
@@ -1,9 +1,45 @@
 package ann
 
 import (
+	"fmt"
 	"math/rand"
+	"reflect"
 	"testing"
 )
+
+func TestMRPTVsExhaustive(t *testing.T) {
+	// Compare the accuracy of an MRPT-based ANNer vs a naive search
+	n := 1000
+	d := 10
+
+	xs := randomMatrix(n, d)
+
+	enn := NewExhaustiveNNer(xs)
+
+	trees := 5
+	depth := 5
+	nn := NewMRPTANNer(trees, depth, xs)
+
+	k := 1
+	sameResultCount := 0
+
+	expNum := 1000
+	for i := 0; i < expNum; i++ {
+		q := randomVector(d)
+
+		indicesANN := nn.ANN(q, k)
+		indicesENN := enn.ANN(q, k)
+
+		if reflect.DeepEqual(indicesANN, indicesENN) {
+			sameResultCount++
+		}
+	}
+
+	// Calculate the hit ratio
+	// Note: The hit ratio heavily depends on the tuning params of the MRPT algo
+	hitRatio := float64(sameResultCount) / float64(expNum)
+	fmt.Println(hitRatio)
+}
 
 func BenchmarkMRPTANNer(b *testing.B) {
 	n := 10000
